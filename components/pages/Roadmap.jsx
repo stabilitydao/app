fd1909e05619d2d882f5bcec3ca399026ae4b0fb
a17 <m@a17.name>
2021-11-02 18:10:33 +0300

    blockchain developer community created

--- components/pages/Roadmap.jsx
+++ components/pages/Roadmap.jsx
@@ -1,5 +1,49 @@
 import React from 'react'
-import {BsDot} from 'react-icons/bs'
+import { BiCircle } from 'react-icons/bi'
+import { TiTick } from 'react-icons/ti'
+
+const phases = [
+    "Inception",
+    "Bootstrapping",
+    "Generation",
+    "Improvement",
+];
+
+const phaseTasks = [
+    {
+        "Blockchain developer community <a href=\"https://t.me/chainbuilders\" target=\"_blank\" className=\"pl-1 underline\" >@chainbuilders</a>": true,
+        "White Paper": false,
+        "Core contracts": false,
+        "App": false,
+        "Testnet deployments": false,
+        "Security audits": false,
+        "Mainnet deployments": false,
+    },
+    {
+        "Liquidity bootstrapping (IDO)": false,
+        "Development Matrix": false,
+        "Formation of Governance": false,
+        "Yield farm project": false,
+        "NFT marketplace project": false,
+        "DeX trading bot": false,
+        "Building projects in accordance with Matrix": false,
+    },
+    {
+        "Incubator construction": false,
+        "Yield Profit Generator (minting farm projects conveyor)": false,
+        "NFT Profit Generator (NFT projects conveyor)": false,
+        "DeX Profit Generator (mechanical trading system, arbitrage protocol)": false,
+        "Building Generators in accordance with Matrix": false,
+        "Stability Fund Development (decentralized Improvement fund)": false,
+        "Chain boilerplate": false,
+    },
+    {
+        "Development of xUnits (offline business managed by DAO through Validators)": false,
+        "TaxPayer company boilerplate": false,
+        "Improvement network": false,
+        "Stable Planet Improvement": false,
+    },
+];
 
 function Roadmap() {
     return (
@@ -7,96 +51,28 @@ function Roadmap() {
             <div className="container w-11/12 py-8">
                 <h1 className="mb-4 text-4xl font-semibold leading-10 tracking-wide text-center text-indigo-500 sm:text-6xl font-Roboto">Roadmap</h1>
                 <div className="max-w-lg mx-auto">
-                    <div className="p-2">
-                        <div className="flex flex-row mb-2">
-                            <BsDot
-                                className="relative text-4xl text-indigo-500 border-2 border-indigo-500 rounded-full -left-4"/>
-                            <h1 className="text-3xl font-Roboto">Phase 0: Inception</h1>
-                        </div>
-                        <div className="p-3 border-l-2 border-indigo-500">
-                            <ul className="text-lg font-semibold ">
-                                {
-                                    [
-                                        "Community",
-                                        "White Paper",
-                                        "Core contracts",
-                                        "App",
-                                        "Testnet deployments",
-                                        "Security audits",
-                                        "Mainnet deployments",
-                                    ].map((data, index) => {
-                                        return <li key={index}>{data}</li>
-                                    })
-                                }
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="p-2">
-                        <div className="flex flex-row mb-2">
-                            <BsDot
-                                className="relative text-4xl text-indigo-500 border-2 border-indigo-500 rounded-full -left-4"/>
-                            <h1 className="text-3xl font-Roboto">Phase 1: Bootstrapping</h1>
-                        </div>
-                        <div className="p-3 border-l-2 border-indigo-500">
-                            <ul className="text-lg font-semibold ">
-                                {
-                                    [
-                                        "Liquidity Bootstrapping (IDO)",
-                                        "Development Matrix",
-                                        "Formation of Governance",
-                                        "Yield Profit Generator (minting farm projects conveyor)",
-                                        "NFT Profit Generator (NFT projects conveyor)",
-                                        "DeX Profit Generator (mechanical trading system, arbitrage protocol)",
-                                    ].map((data, index) => {
-                                        return <li key={index}>{data}</li>
-                                    })
-                                }
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="p-2">
-                        <div className="flex flex-row mb-2">
-                            <BsDot
-                                className="relative text-4xl text-indigo-500 border-2 border-indigo-500 rounded-full -left-4"/>
-                            <h1 className="text-3xl font-Roboto">Phase 2: Generation</h1>
-                        </div>
-                        <div className="p-3 border-l-2 border-indigo-500">
-                            <ul className="text-lg font-semibold ">
-                                {
-                                    [
-                                        "Incubator construction",
-                                        "Building Generators in accordance with Matrix",
-                                        "Stable cross-chain Generation",
-                                        "Stability Fund Development (decentralized Improvement fund)",
-                                        "Chain boilerplate",
-                                    ].map((data, index) => {
-                                        return <li key={index}>{data}</li>
-                                    })
-                                }
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="p-2">
-                        <div className="flex flex-row mb-2">
-                            <BsDot
-                                className="relative text-4xl text-indigo-500 border-2 border-indigo-500 rounded-full -left-4"/>
-                            <h1 className="text-3xl font-Roboto">Phase 3: Improvement</h1>
-                        </div>
-                        <div className="p-3 border-l-2 border-indigo-500">
-                            <ul className="text-lg font-semibold ">
-                                {
-                                    [
-                                        "Development of xUnits (offline business managed by DAO through Validators)",
-                                        "TaxPayer company boilerplate",
-                                        "Improvement network",
-                                        "Stable Planet Improvement",
-                                    ].map((data, index) => {
-                                        return <li key={index}>{data}</li>
-                                    })
-                                }
-                            </ul>
-                        </div>
-                    </div>
+                    {
+                        phases.map((name, index) => {
+                            return (
+                                <div className="p-2" key={index}>
+                                    <div className="flex flex-row mb-2">
+                                        <BiCircle
+                                            className="relative p-1 text-4xl text-indigo-500 bg-indigo-500 border-2 border-indigo-500 rounded-full bg-clip-content -left-4" />
+                                        <h1 className="text-3xl font-Roboto">Phase {index}: {name}</h1>
+                                    </div>
+                                    <div className="p-3 border-l-2 border-indigo-500">
+                                        <ul className="text-lg font-semibold ">
+                                            {
+                                                Object.keys(phaseTasks[index]).map((task, value) => {
+                                                    return phaseTasks[index][task] ? <li key={value}><TiTick className="inline pb-1 text-3xl text-teal-500" /><span dangerouslySetInnerHTML={{__html: task}} /></li> : <li className="pl-7" key={value}><span dangerouslySetInnerHTML={{__html: task}} /></li>
+                                                })
+                                            }
+                                        </ul>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </section>
