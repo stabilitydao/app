3ffdfea782741f48da989848466cca1ecf64d55b
a17 <m@a17.name>
2021-11-06 00:05:22 +0300

    draft dates

--- components/pages/Roadmap.jsx
+++ components/pages/Roadmap.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BiCircle } from 'react-icons/bi'
 import { TiTick } from 'react-icons/ti'
 
-const phases = [
-    "Inception",
-    "Bootstrapping",
-    "Generation",
-    "Improvement",
-];
+const currentPhase = "Inception";
+
+const phases = {
+    "Inception": "Q4 2021",
+    "Bootstrapping": "2022",
+    "Generation": "2023",
+    "Improvement": "2024",
+};
 
 const phaseTasks = [
     {
@@ -34,7 +36,8 @@ const phaseTasks = [
         "NFT Profit Generator (NFT projects conveyor)": false,
         "DeX Profit Generator (mechanical trading system, arbitrage protocol)": false,
         "Building Generators in accordance with Matrix": false,
-        "Stability Fund Development (decentralized Improvement fund)": false,
+        "Improvement Fund (decentralized non-profit fund)": false,
+        "NFT game project": false,
         "Chain boilerplate": false,
     },
     {
@@ -52,19 +55,28 @@ function Roadmap() {
                 <h1 className="mb-4 text-4xl font-semibold leading-10 tracking-wide text-center text-indigo-500 sm:text-6xl font-Roboto">Roadmap</h1>
                 <div className="max-w-lg mx-auto">
                     {
-                        phases.map((name, index) => {
+                        Object.keys(phases).map((name, index) => {
                             return (
                                 <div className="p-2" key={index}>
-                                    <div className="flex flex-row mb-2">
-                                        <BiCircle
-                                            className="relative p-1 text-4xl text-indigo-500 bg-indigo-500 border-2 border-indigo-500 rounded-full bg-clip-content -left-4" />
-                                        <h1 className="text-3xl font-Roboto">Phase {index}: {name}</h1>
+                                    <div className="flex flex-row mb-2 relative">
+                                        <BiCircle className="relative p-1 text-4xl text-indigo-500 bg-indigo-500 border-2 border-indigo-500 rounded-full bg-clip-content -left-4" />
+                                        {currentPhase === name ? (
+                                            <div>
+                                                <h1 className="text-3xl text-indigo-500 font-bold font-Roboto">Phase {index}: {name}</h1>
+                                                <div className="absolute right-0 top-1 text-indigo-500 font-bold text-2xl" >{phases[name]}</div>
+                                            </div>
+                                        ) : (
+                                            <div>
+                                                <h1 className="text-3xl font-Roboto">Phase {index}: {name}</h1>
+                                                <div className="absolute right-0 top-1 font-bold text-2xl" >{phases[name]}</div>
+                                            </div>
+                                        )}
                                     </div>
                                     <div className="p-3 border-l-2 border-indigo-500">
                                         <ul className="text-lg font-semibold ">
                                             {
                                                 Object.keys(phaseTasks[index]).map((task, value) => {
-                                                    return phaseTasks[index][task] ? <li key={value}><TiTick className="inline pb-1 text-3xl text-teal-500" /><span dangerouslySetInnerHTML={{__html: task}} /></li> : <li className="pl-7" key={value}><span dangerouslySetInnerHTML={{__html: task}} /></li>
+                                                    return phaseTasks[index][task] ? <li className="pl-0.5" key={value}><TiTick className="inline pb-1 text-3xl text-teal-500" /><span dangerouslySetInnerHTML={{__html: task}} /></li> : <li className="pl-8" key={value}><span dangerouslySetInnerHTML={{__html: task}} /></li>
                                                 })
                                             }
                                         </ul>
