8e787f839c19f9d05ba0511ca3445c352290df7e
a17 <m@a17.name>
2021-11-18 01:04:45 +0300

    buy by dai

--- components/wallet/swaps.js
+++ components/wallet/swaps.js
@@ -2,7 +2,7 @@ import {MAINNET, RINKEBY, ROPSTEN} from "./networks";
 
 const buyLinks = {
     [MAINNET]: null,
-    [ROPSTEN]: 'https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x29E4d6c08e3AD060Dc2fC8DCE70AaB8C8c57563F',
+    [ROPSTEN]: 'https://app.uniswap.org/#/swap?inputCurrency=0xad6d458402f60fd3bd25163575031acdce07538d&outputCurrency=0x29E4d6c08e3AD060Dc2fC8DCE70AaB8C8c57563F',
     [RINKEBY]: 'https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x7D68ECf1762bA27120d0f98BFcFa0da67eb15860',
 }
 
