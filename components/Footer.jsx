84d5a5e6399e762cc1ced1ca3af9bad397936458
a17 <m@a17.name>
2021-11-22 06:04:03 +0300

    social links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {BsGithub, BsTelegram} from 'react-icons/bs'
+import {BsGithub, BsTelegram, BsTwitter} from 'react-icons/bs'
 
 function Footer() {
     return (
@@ -16,10 +16,13 @@ function Footer() {
                 <div className="w-px bg-gray-400">
                 </div>
                 <div className="self-center">
-                    <a href="https://t.me/chainbuilders" target="_blank" rel="noopener noreferrer" ><BsTelegram className="text-3xl " /></a>
+                    <a href="https://github.com/stabilitydao" target="_blank" rel="noopener noreferrer"><BsGithub className="text-3xl cursor-pointer" /></a>
                 </div>
                 <div className="self-center">
-                    <a href="https://github.com/stabilitydao" target="_blank" rel="noopener noreferrer"><BsGithub className="text-3xl cursor-pointer" /></a>
+                    <a href="https://twitter.com/stabilitydao" target="_blank" rel="noopener noreferrer"><BsTwitter className="text-3xl cursor-pointer" /></a>
+                </div>
+                <div className="self-center">
+                    <a href="https://t.me/stabilitydao" target="_blank" rel="noopener noreferrer" ><BsTelegram className="text-3xl " /></a>
                 </div>
             </div>
         </footer>
