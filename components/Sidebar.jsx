e1c42ba73f3b6be0fa212dacabd77bf31ad97206
a17 <m@a17.name>
2021-12-01 06:13:11 +0300

    pools page layout

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -21,7 +21,7 @@ function Sidebar() {
                 <li><Link href="/"><a className="flex items-center p-4 text-xl gap-x-2 " onClick={() => { dispatch(updateSidebar(false)) }} ><AiFillHome />Home</a></Link></li>
                 <li><Link href="/roadmap"><a className="flex items-center p-4 text-xl gap-x-2 " onClick={() => { dispatch(updateSidebar(false)) }} ><MdEditRoad />Roadmap</a></Link></li>
                 <li><Link href="/ecosystem"><a className="flex items-center p-4 text-xl gap-x-2 " onClick={() => { dispatch(updateSidebar(false)) }} ><BsFillPeopleFill />Ecosystem</a></Link></li>
-                <li><Link href="/pools"><a className="flex items-center p-4 text-xl gap-x-2 " onClick={() => { dispatch(updateSidebar(false)) }} ><BiServer />Staking</a></Link></li>
+                <li><Link href="/pools"><a className="flex items-center p-4 text-xl gap-x-2 " onClick={() => { dispatch(updateSidebar(false)) }} ><BiServer />Pools</a></Link></li>
             </ul>
             <div className="absolute flex flex-col items-center w-60 bottom-2 gap-y-2">
                 <ul className="flex justify-center w-full gap-x-5">
--- components/pages/Pools.jsx
+++ components/pages/Pools.jsx
@@ -2,18 +2,36 @@ import React, { useEffect, useState } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import poolAbi from '@/components/abis/poolAbi'
 import tokenAbi from '@/components/abis/tokenAbi'
-import addresses from 'addresses'
+import addresses, {MAINNET, ROPSTEN, RINKEBY} from 'addresses'
 import { showAlert } from '@/components/common/alert'
+import {useSelector} from "react-redux";
+import {updateIsWalletOption} from "@/redux/slices/modalsSlice";
+import { useDispatch } from 'react-redux'
+
 function Pools() {
+    const dispatch = useDispatch()
     const { account, chainId, library } = useWeb3React()
     const [stakedBalance, setstakedBalance] = useState(0)
     const [Reward, setReward] = useState(0)
     const [stakeNow, setstakeNow] = useState("")
     const [unStakeNow, setunStakeNow] = useState("")
-    const poolAddress = "0xF88f5591beE2C9F05b2D894FC86e86c37F7e869f"
+    const currentNetwork = useSelector(state => state.network.value)
+    const network = chainId ? chainId : currentNetwork
+
+    const pools = {
+        [ROPSTEN]: {
+            PoolV0: {
+                stake: 'PROFIT',
+                earn: 'WETH',
+                contract: "0xF88f5591beE2C9F05b2D894FC86e86c37F7e869f",
+            },
+        },
+    };
+
+    const poolAddress = pools[network] ? pools[network][Object.keys(pools[network])[0]].contract : null
 
     useEffect(() => {
-        if (account) {
+        if (account && poolAddress) {
             const poolContract = new library.eth.Contract(poolAbi, library.utils.toChecksumAddress(poolAddress));
             poolContract.methods.userInfo(account).call().then((staked) => {
                 console.log(staked)
@@ -75,28 +93,55 @@ function Pools() {
     return (
         <section className="dark:bg-black dark:text-white h-calc">
             <div className="container p-4">
-                <div className="max-w-sm p-5 bg-white shadow-2xl w-96 rounded-3xl dark:bg-gray-900">
-                    <div className="flex items-center justify-between mb-2 text-center dark:text-white">
-                        <h2 className="text-2xl font-Roboto">POOL</h2>
-                        <h2 className="text-2xl font-Roboto">PROFIT/WETH</h2>
-                    </div>
-                    <div className="p-2 bg-gray-100 rounded-lg dark:bg-gray-800" >
-                        <h1 className=" font-Roboto">Staking: {Math.floor((stakedBalance * 2.32925) * 1000) / 1000} PROFIT</h1>
-                        <div className="flex flex-row items-center justify-between mb-2">
-                            <h1 className=" font-Roboto">Earned: {Math.floor(Reward * 1000) / 1000} ETH</h1>
-                            <button className="btn" onClick={harvest}>Harvest</button>
+                <h1 className="mb-10 text-4xl font-semibold leading-10 tracking-wide text-center text-indigo-500 sm:text-6xl font-Roboto">Pools</h1>
+                <div>
+                    {pools[network] ? (
+                        <div>
+                            {Object.keys(pools[network]).map(name => {
+                                const pool = pools[network][name]
+                                return (
+                                    <div key={name} className="flex justify-center">
+                                        <div className="flex flex-col bg-white shadow-2xl w-full md:w-2/3 lg:w-1/2 xl:w-2/5 rounded-3xl overflow-hidden dark:bg-gray-900">
+                                            <div className="dark:bg-gray-800 text-3xl p-3 text-center">{name}</div>
+                                            <div className="p-5">Stake {pool.stake} to earn {pool.earn}</div>
+                                            {!account ? (
+                                                <div className="text-center p-5">
+                                                    <button
+                                                        type="button"
+                                                        className="w-40 h-10 btn rounded-2xl"
+                                                        id="options-menu"
+                                                        onClick={() => dispatch(updateIsWalletOption(true))}
+                                                    >
+                                                        Connect Wallet
+                                                    </button>
+                                                </div>
+                                            ) : (
+                                                <div className="p-5">
+                                                    <h1 className=" font-Roboto">Staking: {Math.floor(stakedBalance * 100000) / 100000} PROFIT</h1>
+                                                    <div className="flex flex-row items-center justify-between mb-2">
+                                                        <h1 className=" font-Roboto">Earned: {Math.floor(Reward * 100000) / 100000} ETH</h1>
+                                                        <button className="btn" onClick={harvest}>Harvest</button>
+                                                    </div>
+                                                    <div className="flex flex-row items-center justify-between mb-2">
+                                                        <input type="number" onChange={(e) => { setstakeNow(e.target.value) }} value={stakeNow} className="px-4 py-1 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" min="0" />
+                                                        <button className="btn" onClick={stake}>Stake</button>
+                                                    </div>
+                                                    <div className="flex flex-row items-center justify-between mb-2">
+                                                        <input type="number" onChange={(e) => { setunStakeNow(e.target.value) }} value={unStakeNow} className="px-4 py-1 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" min="0" />
+                                                        <button className="btn" onClick={unStake}>UnStake</button>
+                                                    </div>
+                                                </div>
+                                            )}
+                                        </div>
+                                    </div>
+                                );
+                            })}
                         </div>
-                        <div className="flex flex-row items-center justify-between mb-2">
-                            <input type="number" onChange={(e) => { setstakeNow(e.target.value) }} value={stakeNow} className="px-4 py-1 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" min="0" />
-                            <button className="btn" onClick={stake}>Stake</button>
-                        </div>
-                        <div className="flex flex-row items-center justify-between mb-2">
-                            <input type="number" onChange={(e) => { setunStakeNow(e.target.value) }} value={unStakeNow} className="px-4 py-1 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" min="0" />
-                            <button className="btn" onClick={unStake}>UnStake</button>
-                        </div>
-                    </div>
+                    ) :(
+                        <div className="text-2xl text-center m-6">We currently have no pools on this network</div>
+                    )}
                 </div>
-            </div >
+            </div>
         </section>
     )
 }
