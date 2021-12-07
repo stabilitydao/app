3f1ffdf0868de4ae3d10a32f50be6a35d5d169b8
a17 <m@a17.name>
2021-12-08 00:23:57 +0300

    roadmap update

--- src/components/view/Roadmap.jsx
+++ src/components/view/Roadmap.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BiCircle } from 'react-icons/bi'
 import { TiTick } from 'react-icons/ti'
 
 export const currentPhase = "Inception";
@@ -7,8 +6,8 @@ export const currentPhase = "Inception";
 export const phases = {
     "Inception": "Q4 2021",
     "Bootstrapping": "2022",
-    "Generation": "2023",
-    "Improvement": "2024",
+    "Generation": "2023-2028",
+    "Improvement": "2029",
 };
 
 export const phaseTasks = [
@@ -16,17 +15,16 @@ export const phaseTasks = [
         "Developer community <a href=\"https://t.me/chainbuilders\" target=\"_blank\" className=\"pl-1\">@chainbuilders</a>": true,
         "User community <a href=\"https://t.me/stabilitydao\" target=\"_blank\" className=\"pl-1\">@stabilitydao</a>": true,
         "Stability (PROFIT) token <a href=\"https://etherscan.io/token/0x3fa5F9c876BEbB41B8924633850b1a9922f7E4F9\" target=\"_blank\" className=\"pl-1\">0x3f..F9</a>": true,
-        "White Paper": false,
+        "Dividend payer": false,
+        "White paper": false,
         "App": false,
     },
     {
         "Liquidity bootstrapping (IDO)": false,
         "Bridging token to target chains": false,
         "Governance": false,
-        "Staking pool": false,
+        "AMM V1": false,
         "Development Matrix": false,
-        "AMM": false,
-        "Yield farm project": false,
         "NFT minting project": false,
         "DeX trading bot": false,
         "Building projects in accordance with Matrix": false,
@@ -43,7 +41,7 @@ export const phaseTasks = [
         "Chain boilerplate": false,
     },
     {
-        "Development of xUnits (offline business managed by DAO through Validators)": false,
+        "Development of xUnits (offline business managed by DAO through validators)": false,
         "TaxPayer company boilerplate": false,
         "Improvement network": false,
         "Stable Planet Improvement": false,
@@ -53,32 +51,31 @@ export const phaseTasks = [
 function Roadmap() {
     return (
         <section className="dark:bg-black dark:text-white h-calc">
-            <div className="container p-4">
+            <div className="container p-4 pt-8">
                 <h1 className="mb-4 text-4xl font-semibold leading-10 tracking-wide text-center text-indigo-500 sm:text-6xl font-Roboto">Roadmap</h1>
                 <div className="max-w-lg mx-auto">
                     {
                         Object.keys(phases).map((name, index) => {
                             return (
-                                <div className="p-2" key={index}>
-                                    <div className="relative flex flex-row mb-2">
-                                        <BiCircle className="relative p-1 text-4xl text-indigo-500 bg-indigo-500 border-2 border-indigo-500 rounded-full bg-clip-content -left-4" />
+                                <div className="py-2 my-6" key={index}>
+                                    <div className="flex flex-row mb-2">
                                         {currentPhase === name ? (
-                                            <div>
-                                                <h1 className="text-3xl font-bold text-indigo-500 font-Roboto">Phase {index}: {name}</h1>
-                                                <div className="absolute right-0 text-2xl font-bold text-indigo-500 top-1" >{phases[name]}</div>
+                                            <div className="flex justify-between items-center w-full flex-wrap">
+                                                <h1 className="text-2xl sm:text-3xl font-bold text-indigo-500 font-Roboto">Phase {index}: {name}</h1>
+                                                <div className="text-2xl sm:text-3xl text-indigo-500" >{phases[name]}</div>
                                             </div>
                                         ) : (
-                                            <div>
-                                                <h1 className="text-3xl font-Roboto">Phase {index}: {name}</h1>
-                                                <div className="absolute right-0 text-2xl font-bold top-1" >{phases[name]}</div>
+                                            <div className="flex justify-between items-center w-full flex-wrap">
+                                                <h1 className="text-2xl sm:text-3xl font-bold dark:text-gray-200">Phase {index}: {name}</h1>
+                                                <div className="text-2xl sm:text-3xl" >{phases[name]}</div>
                                             </div>
                                         )}
                                     </div>
-                                    <div className="p-3 border-l-2 border-indigo-500">
-                                        <ul className="text-lg font-semibold ">
+                                    <div className="pb-3 pt-1">
+                                        <ul className="text-lg font-semibold dark:text-gray-200">
                                             {
                                                 Object.keys(phaseTasks[index]).map((task, value) => {
-                                                    return phaseTasks[index][task] ? <li className="relative pl-8" key={value}><TiTick className="absolute left-2 inline  top-0.5 text-2xl text-teal-500" /><span dangerouslySetInnerHTML={{__html: task}} /></li> : <li className="pl-8" key={value}><span dangerouslySetInnerHTML={{__html: task}} /></li>
+                                                    return phaseTasks[index][task] ? <li className="relative py-0.5 pl-5" key={value}><TiTick className="absolute -left-1.5 inline  top-1 text-2xl text-teal-500" /><span dangerouslySetInnerHTML={{__html: task}} /></li> : <li className="py-0.5 pl-5" key={value}><span dangerouslySetInnerHTML={{__html: task}} /></li>
                                                 })
                                             }
                                         </ul>
