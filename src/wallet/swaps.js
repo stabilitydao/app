3757fef2677f9a84566a783bd5cf6c3c8e50d3d7
a17 <m@a17.name>
2021-12-07 15:54:52 +0300

    swaps fix

--- src/wallet/swaps.js
+++ src/wallet/swaps.js
@@ -10,7 +10,6 @@ const lpv3 = {
     [MAINNET]: null,
     [ROPSTEN]: {
         ETH: '0x596B3535C42B6ff7dfF16EdE1f9DfC7e0CA5828A',
-        DAI: '0xCc7c5d318F09918f2A09d2F4BEf75BDeb74f1C16',
     },
     [RINKEBY]: {
         ETH: '0x7393853a7B478bE670B69ead8c3AA2b2D44C7CBC',
