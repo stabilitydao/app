b276bcda82251b3dfb3ad1d79a965ac40cec6f4f
a17 <m@a17.name>
2021-12-14 14:54:39 +0300

    pre-alpha button

--- src/components/AlphaTesting.jsx
+++ src/components/AlphaTesting.jsx
@@ -4,6 +4,7 @@ import { ROPSTEN } from 'addresses'
 import { useDispatch } from "react-redux";
 import { useWeb3React } from "@web3-react/core";
 import { updatenetwork } from "@/redux/slices/networkSlice";
+import {GiCrane} from "react-icons/gi";
 
 function AlphaTesting() {
     const
@@ -21,12 +22,14 @@ function AlphaTesting() {
             <div className="relative group ">
                 <div className="absolute -inset-1 bg-gradient-to-r from-indigo-700 to-teal-400 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 " />
                 {account ? (
-                    <button className="relative px-7 py-4 text-white dark:bg-[#312e81] bg-indigo-700 rounded-lg leading-none flex items-center " onClick={() => { switchNetwork(networks[ROPSTEN], dispatch, library) }}>
-                        Alpha Testing at Ropsten testnet
+                    <button className="relative px-5 py-4 text-white dark:bg-[#551069] bg-indigo-700 rounded-lg leading-none flex items-start " onClick={() => { switchNetwork(networks[ROPSTEN], dispatch, library) }}>
+                        <GiCrane className="text-5xl mr-5 self-center"/>
+                        <span className="text-2xl">Pre-Alpha Testing <br />Ropsten testnet</span>
                     </button>
                 ) : (
-                    <button className="relative px-7 py-4 text-white dark:border-2 dark:border-[#4440b1] dark:bg-[#312e81] bg-indigo-700 rounded-lg leading-none flex items-center " onClick={() => { setNetwork(ROPSTEN) }}>
-                        Alpha Testing at Ropsten testnet
+                    <button className="relative px-5 py-4 text-white dark:bg-[#551069] bg-indigo-700 rounded-lg leading-none flex items-start " onClick={() => { setNetwork(ROPSTEN) }}>
+                        <GiCrane className="text-5xl mr-5 self-center"/>
+                        <span className="text-2xl">Pre-Alpha Testing <br />Ropsten testnet</span>
                     </button>
                 )}
             </div>
