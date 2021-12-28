a9ac01c99cc947e7510ce74997de580e34bf5548
Rajkumar9654 <rajkumarcoolx@gmail.com>
2021-12-28 15:05:01 +0530

    tailwind v2 -> v3

--- src/components/modal/submodals/NetworkOption.jsx
+++ src/components/modal/submodals/NetworkOption.jsx
@@ -18,20 +18,20 @@ function NetworkOption({ onClose }) {
     }
 
     return (
-        <div className="flex flex-col pt-2 bg-white gap-y-2 dark:bg-blue-gray-900">
+        <div className="flex flex-col pt-2 bg-white gap-y-2 dark:bg-slate-900">
             {
                 networkOrder.map((id, index) => {
                     const net = networks[id]
                     if (account && Sync &&Object.keys(networks).includes(chainId ? chainId.toString() : currentNetwork.toString())) {
                         if (net.chainid === chainId) {
-                            return <button onClick={() => { switchNetwork(net, dispatch, library, onClose()) }} key={index} className="block px-4 py-3 text-gray-900 bg-gray-300 rounded-xl text-md dark:text-white dark:bg-blue-gray-700">
+                            return <button onClick={() => { switchNetwork(net, dispatch, library, onClose()) }} key={index} className="block px-4 py-3 text-gray-900 bg-gray-300 rounded-xl text-md dark:text-white dark:bg-slate-700">
                                 <span className="flex items-center font-Roboto">
                                     <span style={{ backgroundColor: net.color, }} className="w-3 h-3 rounded-full mr-3.5" />
                                     {net.fullname}
                                 </span>
                             </button>
                         } else {
-                            return <button onClick={() => { switchNetwork(net, dispatch, library, onClose()) }} key={index} className="block px-4 py-3 text-black bg-gray-100 rounded-xl text-md hover:bg-gray-300 dark:text-white dark:bg-blue-gray-800 dark:hover:bg-blue-gray-700">
+                            return <button onClick={() => { switchNetwork(net, dispatch, library, onClose()) }} key={index} className="block px-4 py-3 text-black bg-gray-100 rounded-xl text-md hover:bg-gray-300 dark:text-white dark:bg-slate-800 dark:hover:bg-slate-700">
                                 <span className="flex items-center font-Roboto">
                                     <span style={{ backgroundColor: net.color, }} className="w-3 h-3 rounded-full mr-3.5" />
                                     {net.fullname}
@@ -40,14 +40,14 @@ function NetworkOption({ onClose }) {
                         }
                     } else {
                         if (net.chainid === currentNetwork) {
-                            return <button onClick={() => { setNetwork(net.chainid) }} key={index} className="block px-4 py-3 text-gray-900 bg-gray-300 rounded-xl text-md dark:text-white dark:bg-blue-gray-700">
+                            return <button onClick={() => { setNetwork(net.chainid) }} key={index} className="block px-4 py-3 text-gray-900 bg-gray-300 rounded-xl text-md dark:text-white dark:bg-slate-700">
                                 <span className="flex items-center font-Roboto ">
                                     <span style={{ backgroundColor: net.color, }} className="w-3 h-3 rounded-full mr-3.5" />
                                     {net.fullname}
                                 </span>
                             </button>
                         } else {
-                            return <button onClick={() => { setNetwork(net.chainid) }} key={index} className="block px-4 py-3 text-black bg-gray-100 rounded-xl text-md hover:bg-gray-300 dark:text-white dark:bg-blue-gray-800 dark:hover:bg-blue-gray-700">
+                            return <button onClick={() => { setNetwork(net.chainid) }} key={index} className="block px-4 py-3 text-black bg-gray-100 rounded-xl text-md hover:bg-gray-300 dark:text-white dark:bg-slate-800 dark:hover:bg-slate-700">
                                 <span className="flex items-center font-Roboto">
                                     <span style={{ color: net.color, }} className="w-2 h-2" />
                                     <span style={{ backgroundColor: net.color, }} className="w-3 h-3 rounded-full mr-3.5" />
