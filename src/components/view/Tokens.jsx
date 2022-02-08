a6b0dc86cd8162c8574e290a374e2a7d18ec047d
Alien Deployer <m@a17.name>
2022-02-08 11:40:10 +0300

    fix heading

--- src/components/view/Tokens.jsx
+++ src/components/view/Tokens.jsx
@@ -109,7 +109,7 @@ function Tokens() {
     return (
         <section className=" h-calc">
             <div className="container p-4 pt-24 lg:pt-4">
-                <h1 className="mb:d-none mb-4 text-4xl font-semibold leading-10 tracking-wide text-center text-indigo-500 sm:text-5xl font-Roboto">PROFIT token</h1>
+                <h1 className="mb:d-none mb-4 text-4xl font-semibold leading-10 tracking-wide text-center text-indigo-500 sm:text-5xl font-Roboto">PROFIT</h1>
                 <article className="mb-10">
                     <div className="mb-10">
                         <div dangerouslySetInnerHTML={{ __html: isDark ? "<style>#dexscreener-embed{position:relative;width:100%;padding-bottom:125%;}@media(min-width:1400px){#dexscreener-embed{padding-bottom:45%;}}#dexscreener-embed iframe{position:absolute;width:100%;height:100%;top:0;left:0;border:0;}</style><div id=\"dexscreener-embed\"><iframe src=\"https://dexscreener.com/polygon/0xd3B1f11f0ff29Add929941095C696D464D6961FC?embed=1&theme=dark&trades=0&info=0\"></iframe></div>" : "<style>#dexscreener-embed{position:relative;width:100%;padding-bottom:125%;}@media(min-width:1400px){#dexscreener-embed{padding-bottom:45%;}}#dexscreener-embed iframe{position:absolute;width:100%;height:100%;top:0;left:0;border:0;}</style><div id=\"dexscreener-embed\"><iframe src=\"https://dexscreener.com/polygon/0xd3B1f11f0ff29Add929941095C696D464D6961FC?embed=1&trades=0&info=0\"></iframe></div>" }} />
