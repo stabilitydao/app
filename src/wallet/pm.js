87aaaaf681d3e8b169989b1675e616ba831d67db
Alien Deployer <m@a17.name>
2022-03-20 19:13:18 +0300

    pm colors 3-5

--- src/wallet/pm.js
+++ src/wallet/pm.js
@@ -7,6 +7,18 @@ const pmColors = {
         name: 'Ambient Red',
         rgb: '#fd4747',
     },
+    3: {
+        name: 'Royal Purple',
+        rgb: '#8447fd',
+    },
+    4: {
+        name: 'Fresh Blueberries',
+        rgb: '#2b44fd',
+    },
+    5: {
+        name: 'Gentle Olive',
+        rgb: '#00f7f7',
+    },
 }
 
 export {pmColors}
