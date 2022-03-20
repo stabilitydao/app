7ef12597e7d2997d0a2ea0f96351aafa508b0e3c
Rajkumar9654 <rajkumarcoolx@gmail.com>
2022-03-20 23:49:54 +0530

    profitmaker

---
+++ src/components/view/ProfitMaker.jsx
@@ -0,0 +1,175 @@
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { ethers } from 'ethers'
+import { BsStopwatch } from 'react-icons/bs'
+import addresses from '@stabilitydao/addresses'
+import WEB3 from '@/src/functions/web3'
+import { networks } from '../../wallet'
+import pmAbi from '@/src/abis/pmAbi'
+import tokenAbi from '@/src/abis/tokenAbi'
+import { useDispatch, useSelector } from "react-redux";
+import { pmtotalSupply, mintStart, mintEnd, toMint } from "@/redux/slices/pmSlice";
+import { useWeb3React } from '@web3-react/core'
+import { txConfirmedByNetwork, updateIsTxSubmitted, updateIsWaitingForWalletTxConfirm } from "@/redux/slices/modalsSlice";
+import { showAlert } from '@/src/components/alert'
+
+function ProfitMaker() {
+    const [Choose, setChoose] = useState(false)
+    const [SelectedNft, setSelectedNft] = useState(null)
+    const [leftNfts, setleftNfts] = useState(null)
+    const [isApproved, setisApproved] = useState(null)
+    const web3 = WEB3()
+    const currentNetwork = useSelector(state => state.network.value)
+    const { library, active, chainId, account } = useWeb3React()
+    const network = chainId && networks[chainId] ? chainId : currentNetwork
+    const rpcLib = chainId ? library : web3
+    const pm = useSelector(state => state.pm)
+    const dispatch = useDispatch();
+    async function remainingNft() {
+        const res = await fetch('/api/maker-testnet/available-colors')
+        setleftNfts(await res.json())
+    }
+    async function handleIsApproved() {
+        if (active) {
+            const tokenContract = new rpcLib.eth.Contract(tokenAbi, addresses[network].token)
+            const allowedTokens = await tokenContract.methods.allowance(account, addresses[network].pm).call()
+            if (allowedTokens >= 10000) {
+                setisApproved(true)
+            } else {
+                setisApproved(false)
+            }
+        }
+    }
+    async function handleApprove() {
+        if (!isApproved && active) {
+            dispatch(updateIsWaitingForWalletTxConfirm(true))
+            try {
+                const profitContract = new rpcLib.eth.Contract(tokenAbi, addresses[network].token)
+                profitContract.methods.approve(addresses[network].pm, ethers.constants.MaxUint256).send({ from: account }).on('transactionHash', txhash => {
+                    dispatch(updateIsWaitingForWalletTxConfirm(false))
+                    dispatch(updateIsTxSubmitted(txhash))
+                }).on('receipt', r => {
+                    dispatch(txConfirmedByNetwork())
+                })
+                handleIsApproved()
+            } catch (error) {
+                console.log(err)
+                dispatch(updateIsWaitingForWalletTxConfirm(false))
+            }
+        } else {
+            showAlert("Failed")
+        }
+    }
+    async function handleMint() {
+        if (active) {
+            dispatch(updateIsWaitingForWalletTxConfirm(true))
+            try {
+                const pmContract = new rpcLib.eth.Contract(pmAbi, addresses[network].pm)
+                pmContract.methods.safeMint(account, SelectedNft).send({ from: account }).on('transactionHash', txhash => {
+                    dispatch(updateIsWaitingForWalletTxConfirm(false))
+                    dispatch(updateIsTxSubmitted(txhash))
+                }).on('receipt', r => {
+                    dispatch(txConfirmedByNetwork())
+                })
+                remainingNft()
+            } catch (error) {
+                console.log(err)
+                dispatch(updateIsWaitingForWalletTxConfirm(false))
+            }
+        } else {
+            showAlert("Failed")
+        }
+    }
+    useEffect(() => {
+        if (addresses[network].pm) {
+            // ABI is ERC-721
+            let contract;
+            contract = new rpcLib.eth.Contract(pmAbi, addresses[network].pm);
+            contract.methods.totalSupply().call().then((r) => {
+                dispatch(pmtotalSupply(r))
+            })
+            contract.methods.mintingStart().call().then((r) => {
+                dispatch(mintStart(r))
+            })
+            contract.methods.mintingEnd().call().then((r) => {
+                dispatch(mintEnd(r))
+            })
+            contract.methods.toMint().call().then((r) => {
+                dispatch(toMint(r))
+            })
+        };
+        remainingNft()
+        handleIsApproved()
+    }, [network])
+    useEffect(() => {
+        if (leftNfts) {
+            setSelectedNft(Object.entries(leftNfts)[0][0])
+        }
+    }, [leftNfts])
+
+    return (
+        <section className=" h-calc">
+            <div className="container p-4 pt-24 lg:pt-4">
+                <h1 className="mb:d-none mb-4 text-4xl font-semibold leading-10 tracking-wide text-center text-indigo-500 sm:text-5xl font-Roboto">Profit Maker</h1>
+                <div className="flex flex-col md:flex-row ">
+                    <div className="flex-1 p-4">
+                        <img className='w-full' src="/pm.gif" alt="" height='200' width='200' />
+                    </div>
+                    <div className="flex-1 p-4 font-Roboto">
+                        <div className='flex flex-col '>
+                            <h1 className='text-2xl leading-normal border-2 border-indigo-800 p-4  rounded-t-xl'>
+                                <BsStopwatch className='inline mr-2' />
+                                Minting ends {network && addresses[network].pm && pm && pm.mintEnd > 0 ? (new Date(pm.mintEnd * 1000)).toString() : '-'}
+                            </h1>
+                            <div className='border-indigo-800 border-2 rounded-b-xl p-4 '>
+                                <p className='text-xl leading-normal mb-8'>
+                                    {network && addresses[network].pm && pm && pm.toMint > 0 ? pm.toMint : '-'} left
+                                </p>
+                                <p className='text-xl leading-normal '>
+                                    Current price
+                                </p>
+                                <div className='flex flex-row items-center gap-x-2 text-2xl mb-8'>
+                                    <Image src='/profit.png' width='25' height='25' alt="Not present" className='' />
+                                    <p className=''>
+                                        10,000
+                                    </p>
+                                </div>
+                                {
+                                    leftNfts &&
+                                    <div className="relative inline-block z-0 mb-8">
+                                        <button onClick={() => { setChoose(!Choose) }} className="relative btn z-10 block p-2   border border-transparent rounded-md   focus:outline-none">
+                                            {leftNfts && SelectedNft ? leftNfts[SelectedNft].name : ""}
+                                        </button>
+                                        <div className={`absolute z-0 right-0  w-48 py-2 mt-2 bg-white rounded-md shadow-xl dark:bg-gray-800 ${Choose ? 'block' : 'hidden'}`}>
+                                            {
+                                                Object.entries(leftNfts).map((nft, index) => {
+                                                    return (
+                                                        <div onClick={() => { setChoose(false); setSelectedNft(nft[0]) }} key={index} className=" flex flex-row justify-between cursor-pointer px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                                                            {nft[1].name}
+                                                            <div className={`h-4 w-8`} style={{ backgroundColor: nft[1].rgb }}>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                    </div>
+                                }
+                                <div className="h-16 space-y-4">
+                                    {
+                                        isApproved ?
+                                            <button className='btn w-full text-2xl rounded-md' onClick={handleMint}>MINT</button>
+                                            :
+                                            <button className='btn w-full text-2xl rounded-md' onClick={() => { handleApprove() }}>APPROVE</button>
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ProfitMaker
---
+++ src/pages/profitmaker.js
@@ -0,0 +1,7 @@
+import { ProfitMaker } from '../components/view'
+
+function profitmaker() {
+    return <ProfitMaker />
+}
+
+export default profitmaker
